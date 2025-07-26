e6b5fbf6a481f13771eeede979fe0fd4f6a1aa98
Noel Sebu <noelsebu555@gmail.com>
2025-07-26 22:27:47 +0530

    is logged in middleware added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+
+// middleware
+const { isUserLoggedIn } = require("../middleware/auth.middleware");
+
+// controllers
 const { userLogin } = require("../controllers/auth.controller");
 
 // login user
-router.post("/login", userLogin);
+router.post("/login", isUserLoggedIn, userLogin);
 
 module.exports = router;
