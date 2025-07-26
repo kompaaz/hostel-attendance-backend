4c2ad59bb30ea135f925283ffea208bbf390115c
Noel Sebu <noelsebu555@gmail.com>
2025-07-26 11:34:26 +0530

    student controller created

---
+++ controllers/student.controller.js
@@ -0,0 +1,15 @@
+const Student = require("../models/student.model");
+
+const getStudents = async (req, res) => {
+  try {
+    console.log("trying to get students");
+    const adId = req.query.adId; // e.g., /students?adId=AD001
+    const students = await Student.find({ assignedAd: adId }); // Filter by assigned AD
+    res.json({ students });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error fetching students" });
+  }
+};
+
+module.exports = { getStudents };
