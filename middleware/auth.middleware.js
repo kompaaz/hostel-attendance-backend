5a27ec7e537595de66c1678342c93baccee7adcf
Noel Sebu <noelsebu555@gmail.com>
2025-07-26 21:58:05 +0530

    middleware name changed and export modified

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,9 +1,11 @@
-// middleware/auth.js
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
+  console.log("came into middleware");
+
+  const token = req.cookies.token; // ✅ FIXED
+  console.log(token);
 
   if (!token) {
     return res.status(401).json({ error: "Access denied. No token provided." });
@@ -11,11 +13,11 @@ const verifyToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Now you can access `req.user.username` in routes
+    req.token = decoded;
     next();
   } catch (err) {
     res.status(401).json({ error: "Invalid token." });
   }
 };
 
-module.exports = verifyToken;
+module.exports = { verifyToken };
