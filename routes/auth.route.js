1dc6edddf6a9fa0ff647ec1139f2b9b0ccf4f9a9
Noel Sebu <noelsebu555@gmail.com>
2025-07-27 14:02:33 +0530

    removed attendance from auth route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,7 +7,6 @@ const { isUserLoggedIn } = require("../middleware/auth.middleware");
 
 // controllers
 const { userLogin } = require("../controllers/auth.controller");
-const { route } = require("./auth.route");
 
 // login user
 router.get("/login", async (req, res) => {
@@ -20,30 +19,5 @@ router.get("/student_list", async (req, res) => {
   const students = await Students.find();
   res.render("student_list", { users: students });
 });
-
-router.get("/ad_attendance", async (req, res) => {
-  try {
-    const students = await Students.find();
-
-    // Group by roomNo
-    const groupedUsers = {};
-
-    students.forEach((student) => {
-      const room = student.roomNo || "Unknown";
-      if (!groupedUsers[room]) {
-        groupedUsers[room] = [];
-      }
-      groupedUsers[room].push(student);
-    });
-    console.log(groupedUsers);
-    res.render("ad_attendance", { groupedUsers });
-  } catch (error) {
-    console.error("Error fetching students:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-
-
 
 module.exports = router;
