ca2775c6436d84c9279f432c4d2f11f0afb49c5b
Noel Sebu <noelsebu555@gmail.com>
2025-07-27 15:09:13 +0530

    create a new user in script

--- scripts/insertUsers.js
+++ scripts/insertUsers.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+const User = require("../models/user.model");
 const Student = require("../models/student.model");
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/hostel_attendance";
@@ -11,22 +11,32 @@ async function insertInitialUsers() {
 
   const users = [
     {
-      username: "director_father",
-      password: await bcrypt.hash("father123", 10),
-      role: "director",
-    },
-    {
-      username: "ad_john",
-      password: await bcrypt.hash("john123", 10),
+      username: "Augustin",
+      password: await bcrypt.hash("1234", 10),
       role: "ad",
-      hall: "St Paul Hall",
+      roomsIncharge: {
+        hall: ["APH", "SH"],
+        from: 21, // start room number
+        to: 48, // end room number
+      },
     },
-    {
-      username: "ad_mary",
-      password: await bcrypt.hash("mary123", 10),
-      role: "ad",
-      hall: "St Thomas Hall",
-    },
+    // {
+    //   username: "director_father",
+    //   password: await bcrypt.hash("father123", 10),
+    //   role: "director",
+    // },
+    // {
+    //   username: "ad_john",
+    //   password: await bcrypt.hash("john123", 10),
+    //   role: "ad",
+    //   hall: "St Paul Hall",
+    // },
+    // {
+    //   username: "ad_mary",
+    //   password: await bcrypt.hash("mary123", 10),
+    //   role: "ad",
+    //   hall: "St Thomas Hall",
+    // },
   ];
 
   const students = [
@@ -68,8 +78,8 @@ async function insertInitialUsers() {
   ];
 
   // await User.deleteMany({}); // Optional: clear old users
-  // await User.insertMany(users);
-  await Student.insertMany(students);
+  await User.insertMany(users);
+  // await Student.insertMany(students);
   console.log("Users inserted successfully");
 
   mongoose.disconnect();
