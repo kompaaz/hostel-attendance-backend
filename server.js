bbd334c63dd6e641fbf20f7a74b20e51d2376894
Noel Sebu <noelsebu555@gmail.com>
2025-07-27 11:00:58 +0530

    url encoded middleware added

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
