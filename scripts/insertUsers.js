b5eafcacc09ccdbf08724ac856c32cbd184c984e
Joerakesh <rakeshjoe52@gmail.com>
2025-07-27 16:10:53 +0530

    added rex auth

--- scripts/insertUsers.js
+++ scripts/insertUsers.js
@@ -10,23 +10,33 @@ async function insertInitialUsers() {
   console.log("Connected to MongoDB");
 
   const users = [
+    // {
+    //   username: "Augustin",
+    //   password: await bcrypt.hash("1234", 10),
+    //   role: "ad",
+    //   roomsIncharge: {
+    //     hall: ["APH", "SH"],
+    //     from: 21, // start room number
+    //     to: 48, // end room number
+    //   },
+    // },
+    // {
+    //     username: "Philips",
+    //     password: await bcrypt.hash("1234", 10),
+    //     role: "ad",
+    //     roomsIncharge: {
+    //         from: 49, // start room number
+    //         to: 100, // end room number
+    //     }
+    // },
     {
-      username: "Augustin",
-      password: await bcrypt.hash("1234", 10),
-      role: "ad",
-      roomsIncharge: {
-        hall: ["APH", "SH"],
-        from: 21, // start room number
-        to: 48, // end room number
-      },
-    },
-    {
-        username: "Philips",
+        username: "Rex",
         password: await bcrypt.hash("1234", 10),
         role: "ad",
         roomsIncharge: {
-            from: 49, // start room number
-            to: 100, // end room number
+            hall: ["Beschi Hall", "Britto Hall", "Loyola Hall", "Xavier Hall"],
+            from: 2,
+            to: 20,
         }
     }
   ];
