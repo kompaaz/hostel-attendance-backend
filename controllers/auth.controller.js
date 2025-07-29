1bd370fd517a282cd6ed1370c25bb1c799986218
Noel Sebu <noelsebu555@gmail.com>
2025-07-29 07:29:20 +0530

    cookie attributes added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -23,20 +23,10 @@ const userLogin = async (req, res) => {
     const token = jwt.sign({ id: user._id }, JWT_SECRET);
     res.cookie("token", token, {
       httpOnly: true,
+      secure: true, // ✅ Required on Vercel (HTTPS)
+      sameSite: "None", // ✅ Required for cross-origin cookie
     });
 
-    // ✅ Login success
-    // res.status(200).json({
-    //   message: "Login successful",
-    //   user: {
-    //     id: user._id,
-    //     role: user.role,
-    //     hall: user.hall || null,
-    //   },
-    // });
-
-    // res.redirect("/api/attendance");
-    // res.redirect("https://sh.devnoel.org/attendance-records");
     res.status(200).json({
       message: "Login successful",
     });
