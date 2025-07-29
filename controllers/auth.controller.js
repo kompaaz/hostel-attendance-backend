349a4fb50d0cbe526b04a1ebce5cc11685b48302
Noel Sebu <noelsebu555@gmail.com>
2025-07-29 08:38:00 +0530

    logout controller created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -36,4 +36,14 @@ const userLogin = async (req, res) => {
   }
 };
 
-module.exports = { userLogin };
+const logout = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true, // ✅ Required on Vercel (HTTPS)
+    sameSite: "None", // ✅ Required for cross-origin cookie
+    maxAge: 0,
+  });
+  res.status(200).json({ message: "logout successfull" });
+};
+
+module.exports = { userLogin, logout };
