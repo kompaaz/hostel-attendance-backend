b405103cd0c606440250ef7426d464159c9ff243
Noel Sebu <noelsebu555@gmail.com>
2025-07-29 10:08:00 +0530

    reuse connection of db

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
   try {
+    if (isConnected) {
+      console.log("✅ Reusing existing database connection");
+      return;
+    }
+
     const URI = process.env.MONGO_URI;
-    if(!URI){
+    if (!URI) {
       console.log("could not get connection URI db.js");
-      throw new Error("URI not defined in .env")
+      throw new Error("URI not defined in .env");
     }
     // console.log(URI);
     await mongoose.connect(URI);
+    isConnected = db.connections[0].readyState === 1;
     console.log("SUCCESSFULLY CONNECTED TO DATABASE");
   } catch (error) {
     console.log(`Error in db.js while connecting to db`);
