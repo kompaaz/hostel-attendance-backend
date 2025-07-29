fc71cc90832327c566c718ab687039ac37fc57de
Noel Sebu <noelsebu555@gmail.com>
2025-07-29 09:15:29 +0530

    student controller and route commented out

--- routes/attendance.route.js
+++ routes/attendance.route.js
@@ -8,11 +8,13 @@ const { verifyToken } = require("../middleware/auth.middleware");
 const {
   markAttendance,
   getStudentsAccordingToAd,
+  getAttendanceRecords,
 } = require("../controllers/attendance.controller");
-const { getStudents } = require("../controllers/student.controller");
+// const { getStudents } = require("../controllers/student.controller");
 
 router.get("/", verifyToken, getStudentsAccordingToAd); // POST /api/attendance/
 router.post("/mark", verifyToken, markAttendance); // POST /api/attendance/mark
-router.get("/students", getStudents); // GET: Fetch all students based on adId
+router.get("/get-attendance-records", getAttendanceRecords); // GET /api/attendance/get-attendance-records
+// router.get("/students", getStudents); // GET: Fetch all students based on adId
 
 module.exports = router;
