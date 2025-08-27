744f360e32f2f14aba772581ed574931b02e0051
Joerakesh <rakeshjoe52@gmail.com>
2025-08-27 12:31:09 +0530

    for attendance record new method

--- routes/attendance.route.js
+++ routes/attendance.route.js
@@ -12,9 +12,12 @@ const {
 } = require("../controllers/attendance.controller");
 // const { getStudents } = require("../controllers/student.controller");
 
+
+
 router.get("/", verifyToken, getStudentsAccordingToAd); // POST /api/attendance/
 router.post("/mark", verifyToken, markAttendance); // POST /api/attendance/mark
-router.get("/get-attendance-records", getAttendanceRecords); // GET /api/attendance/get-attendance-records
+router.get("/get-attendance-records", verifyToken, getAttendanceRecords);
+// GET /api/attendance/get-attendance-records
 // router.get("/students", getStudents); // GET: Fetch all students based on adId
 
 module.exports = router;
